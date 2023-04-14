526034ced4a0f26f46513ad238495ba5b1483df8
lecture-bisper <bisper@outlook.kr>
2023-04-14 16:54:25 +0900

    1. 인터페이스의 다형성 2. 인터페이스를 필드로 사용한 다형성 3. 인터페이스를 배열로 사용한 다형성

---
+++ java_0414/src/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+//  인터페이스 타입의 변수 선언
+//  해당 변수에 자식 클래스 타입의 객체를 저장
+  Tire frontLeftTire = new HankookTire();
+  Tire frontRightTire = new HankookTire();
+  Tire backLeftTire = new HankookTire();
+  Tire backRightTire = new HankookTire();
+
+  void run() {
+    frontLeftTire.roll();
+    frontRightTire.roll();
+    backLeftTire.roll();
+    backRightTire.roll();
+  }
+}
