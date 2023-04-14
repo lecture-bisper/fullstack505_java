0c75316f8ae666d7fc2604354cb305d513370211
lecture-bisper <bisper@outlook.kr>
2023-04-14 16:41:39 +0900

    1. 문제 풀이

--- java_0413/src/Quiz.java
+++ java_0413/src/Quiz.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
 public class Quiz {
+//  추상 클래스인 Shape 클래스 타입의 배열에 저장되는 데이터 위치
   private static int position = 0;
+//  추상 클래스인 Shape 클래스 타입의 배열 선언
   private static Shape[] shapes = new Shape[10];
 
   public static void quiz02() {
@@ -25,19 +27,26 @@ public class Quiz {
   private static void quiz05() {
     System.out.println("\n ----- Quiz05 -----\n");
 
+//    StringStack 클래스 타입의 변수 선언 및 객체 생성
+//    매개변수로 정수 10을 사용하여 스택 구조로 사용하는 배열의 크기를 10으로 설정
     StringStack stack = new StringStack(10);
     Scanner sc = new Scanner(System.in);
 
     System.out.print(" >> ");
 
+//    5번은 사용자 키보드 입력
     for (int i = 0; i < 5; i++) {
       String text = sc.next();
+//      StringStack 클래스 타입의 객체 stack의 메소드 push를 사용하여 데이터를 입력받은 데이터를 저장
       stack.push(text);
     }
 
+//    StringStack 클래스 타입의 객체 stack의 메소드 length()를 사용하여 스택 구조의 배열 저장된 데이터의 크기를 가져옴
     int count = stack.length();
 
+//    입력된 내용을 출력
     for (int i = 0; i < count; i++) {
+//      StringStack 클래스 타입의 객체 stack의 메소드 pop()을 사용하여 스택 구조의 배열에 저장된 데이터를 하나씩 가져옴
       System.out.print(stack.pop() + " ");
     }
   }
@@ -47,22 +56,31 @@ public class Quiz {
 
     Scanner sc = new Scanner(System.in);
 
+//    메뉴 선택용 변수
     int choice = 0;
 
+//    무한 반복
     while (choice != 4) {
+//      화면에 메뉴 출력
       System.out.print("삽입(1), 삭제(2), 모두 보기(3), 종료(4) >> ");
+//      메뉴 선택
       choice = sc.nextInt();
 
+//      메인 메뉴 선택에 따라 다르게 동작
       switch (choice) {
         case 1:
+//          서브 메뉴 출력
           System.out.print("도형 종류 Line(1), Rect(2), Circle(3) >> ");
+//          서브 메뉴 입력 받기
           int type = sc.nextInt();
 
+//          서브 메뉴 클릭 시 1 미만, 3 초과일 경우 오류 내용 출력
           if (type < 1 || type > 3) {
             System.out.println("잘못 입력하였습니다.");
-            break;
+            break; // 스위치 문 강제 종료, 반복문 재시작
           }
 
+//          Shape 클래스 타입의 배열에 데이터 추가
           insert(type);
           break;
 
@@ -96,9 +114,12 @@ public class Quiz {
 
   }
 
+//  Shapes 배열에 데이터 입력
   public static void insert(int type) {
+//    부모인 추상 클래스 Shape 타입의 변수 선언;
     Shape obj = null;
 
+//    매개변수로 받아온 데이터에 따라 Shape 타입의 변수에 저장할 자식 클래스의 객체를 생성함
     switch (type) {
       case 1:
         obj = new Line();
@@ -113,10 +134,13 @@ public class Quiz {
         break;
     }
 
+//    추상 클래스 타입의 배열 shapes에 데이터 추가
     shapes[position] = obj;
+//    저장된 위치 업데이트
     position++;
   }
 
+//  추상 클래스 타입의 배열 shapes 에 저장된 내용 삭제
   public static boolean delete(int index) {
     int count = 0;
     shapes[index] = null;
--- java_0413/src/StringStack.java
+++ java_0413/src/StringStack.java
@@ -1,34 +1,45 @@
 public class StringStack extends StackClass{
+//  스택 구조로 사용할 배열
   private String[] stack;
+//  스택에 쌓여있는 데이터의 크기(높이)
   private int count;
 
+//  생성자, 객체 생성 시 매개변수로 스택의 크기를 결정
   public StringStack(int index) {
-    stack = new String[index];
-    count = 0;
+    stack = new String[index]; // 스택 크기 지정
+    count = 0; // 스택의 높이를 0으로 초기화
   }
 
+//  스택에 저장된 데이터의 마지막 위치 출력
   @Override
   int length() {
     return count;
   }
 
+//  스택에 저장된 가장 마지막 데이터를 출력
   @Override
   String pop() {
+//    스택의 높이가 0일 경우 저장된 데이터 없음
     if (count == 0) {
       return null;
     }
-    count--;
+    count--; // 스택 높이를 1 낮춤
     return stack[count];
   }
 
+//  스택의 마지막 위치에 데이터를 저장
+//  스택의 최대 크기에 도달 시 더이상 데이터를 쌓을 수 없음(false)
   @Override
   boolean push(String ob) {
+//    스택의 높이와 스택 구조로 사용하는 배열의 크기가 같으면 더이상 데이터를 추가할 수 없으므로 false를 출력
     if (count == stack.length) {
       return false;
     }
 
+//    스택 구조로 사용하는 배열에 데이터를 추가
     stack[count++] = ob;
 
+//    정상적으로 데이터를 저장했다는 의미로 true 출력
     return true;
   }
 }
