8cb9201ef3f18e3dd8ced160d36d4733fcad333a
lecture-bisper <bisper@outlook.kr>
2023-04-25 16:05:36 +0900

    1. 4/10 java 프로젝트 추가 2. 스레드 사용하기

---
+++ java_0419/src/BeepPrintEx1.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+
+public class BeepPrintEx1 {
+  public static void main(String[] args) {
+//    싱글 스레드로 프로그램을 동작했을 경우
+    Toolkit toolkit = Toolkit.getDefaultToolkit();
+
+    for (int i = 0; i < 5; i++) {
+      toolkit.beep();
+      try {
+        Thread.sleep(500);
+      }
+      catch (Exception e) {
+
+      }
+    }
+
+    for (int i = 0; i < 5; i++) {
+      System.out.println("띵");
+      try {
+        Thread.sleep(500);
+      }
+      catch (Exception e) {
+
+      }
+    }
+  }
+}
